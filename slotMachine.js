1b202a4727bce81d73e92c18e524d548f737cfad
Javi <Javierbarcelo2105@hotmail.com>
2025-01-21 22:22:48 +0100

    with css and js

--- slotMachine.js
+++ slotMachine.js
@@ -5,7 +5,7 @@ let tiempoSlot2 = 100;
 let tiempoSlot3 = 100;
 let manejaSlot1, manejaSlot2, manejaSlot3;
 let slotsEnMovimiento = false;
-let monedas = 10; // Inicialmente 10 monedas
+let monedas = 10;
 
 function actualizarMonedas() {
     document.getElementById("recuentoMonedas").innerText = `Monedas: ${monedas}`;
@@ -14,25 +14,25 @@ function actualizarMonedas() {
 function cambiarImagen1() {
     let aleatorio = parseInt(Math.random() * imagenes.length);
     document.getElementById("slot1").src = imagenes[aleatorio];
-    console.log(aleatorio);
+   
 }
 
 function cambiarImagen2() {
     let aleatorio = parseInt(Math.random() * imagenes.length);
     document.getElementById("slot2").src = imagenes[aleatorio];
-    console.log(aleatorio);
+    
 }
 
 function cambiarImagen3() {
     let aleatorio = parseInt(Math.random() * imagenes.length);
     document.getElementById("slot3").src = imagenes[aleatorio];
-    console.log(aleatorio);
+    
 }
 
 function tirada() {
     if (slotsEnMovimiento) return; // Evita que se vuelva a tirar si los slots están en movimiento
     if (monedas <= 0) {
-       document.getElementById("mensajeVictoria").innerHTML = "No tienes monedas para jugar";
+        document.getElementById("mensajeVictoria").innerHTML=("No tienes suficientes monedas para jugar.");
         return;
     }
     monedas--; // Resta una moneda por tirada
@@ -65,33 +65,38 @@ function pararSlot() {
 function verificarGanador() {
     let slot1 = document.getElementById("slot1").src;
     let slot2 = document.getElementById("slot2").src;
-    let slot3 = document.getElementById("slot3").src;
-
+    let slot3 = document.getElementById("slot3").src;   
     if (slot1 === slot2 && slot2 === slot3) {
         let mensaje = "Has ganado! ";
         let premio = 0;
-        switch (slot1) {
-            case imagenes[0]:
-                premio = 100;
-                mensaje += "Bote: 100 monedas";
-                break;
-            case imagenes[1]:
-                premio = 200;
-                mensaje += "Bote: 200 monedas";
-                break;
-            case imagenes[2]:
-                premio = 300;
-                mensaje += "Bote: 300 monedas";
-                break;
-            case imagenes[3]:
-                premio = 400;
-                mensaje += "Bote: 400 monedas";
-                break;
+        if (imagenes[0])
+        {
+            premio = 5;
+        }else if (imagenes[1])
+        {
+            premio = 10;
+        }else if(imagenes[2])
+        {
+            premio = 50;
+        }else if(imagenes[3])
+        {
+            premio = 100;
         }
-        monedas =monedas + premio;
+        console.log(monedas);
+        monedas += premio;
         actualizarMonedas();
+        console.log(monedas);
+        mensaje += `Has ganado ${premio} monedas.`;
         document.getElementById("mensajeVictoria").innerHTML = mensaje;
     } else {
-        document.getElementById("mensajeVictoria").innerHTML = "sueterte la proxima vez";
+        document.getElementById("mensajeVictoria").innerHTML = "Suerte la próxima vez";
     }
+    
+}
+function tarjetazo()
+{
+    let textareaValue = document.getElementById("tarjeta").value;
+    monedas += parseInt(textareaValue,10);
+    console.log(monedas);
+    actualizarMonedas();
 }
