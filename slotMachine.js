794e8a3e2e6f0bdfffd338b6a2f3f72c54e3798e
Javi <Javierbarcelo2105@hotmail.com>
2025-01-21 10:11:39 +0100

    update3

--- slotMachine.js
+++ slotMachine.js
@@ -5,6 +5,11 @@ let tiempoSlot2 = 100;
 let tiempoSlot3 = 100;
 let manejaSlot1, manejaSlot2, manejaSlot3;
 let slotsEnMovimiento = false;
+let monedas = 10; // Inicialmente 10 monedas
+
+function actualizarMonedas() {
+    document.getElementById("recuentoMonedas").innerText = `Monedas: ${monedas}`;
+}
 
 function cambiarImagen1() {
     let aleatorio = parseInt(Math.random() * imagenes.length);
@@ -25,6 +30,18 @@ function cambiarImagen3() {
 }
 
 function tirada() {
+    if (slotsEnMovimiento) return; // Evita que se vuelva a tirar si los slots están en movimiento
+    if (monedas <= 0) {
+        alert("No tienes suficientes monedas para jugar.");
+        return;
+    }
+    monedas--; // Resta una moneda por tirada
+    actualizarMonedas();
+    slotsEnMovimiento = true;
+
+    let slotSound = document.getElementById("slotSound");
+    slotSound.currentTime = 0; 
+    slotSound.play(); 
 
     manejaSlot1 = setInterval(cambiarImagen1, tiempoSlot1);
     manejaSlot2 = setInterval(cambiarImagen2, tiempoSlot2);
@@ -36,4 +53,45 @@ function pararSlot() {
     clearInterval(manejaSlot1);
     clearInterval(manejaSlot2);
     clearInterval(manejaSlot3);
+    slotsEnMovimiento = false; // Permite volver a tirar
+
+    let slotSound = document.getElementById("slotSound");
+    slotSound.pause(); 
+    slotSound.currentTime = 0;
+
+    verificarGanador();
+}
+
+function verificarGanador() {
+    let slot1 = document.getElementById("slot1").src;
+    let slot2 = document.getElementById("slot2").src;
+    let slot3 = document.getElementById("slot3").src;
+
+    if (slot1 === slot2 && slot2 === slot3) {
+        let mensaje = "Has ganado! ";
+        let premio = 0;
+        switch (slot1) {
+            case imagenes[0]:
+                premio = 100;
+                mensaje += "Bote: 100 monedas";
+                break;
+            case imagenes[1]:
+                premio = 200;
+                mensaje += "Bote: 200 monedas";
+                break;
+            case imagenes[2]:
+                premio = 300;
+                mensaje += "Bote: 300 monedas";
+                break;
+            case imagenes[3]:
+                premio = 400;
+                mensaje += "Bote: 400 monedas";
+                break;
+        }
+        monedas += premio;
+        actualizarMonedas();
+        document.getElementById("mensajeVictoria").innerHTML = mensaje;
+    } else {
+        document.getElementById("mensajeVictoria").innerHTML = "";
+    }
 }
